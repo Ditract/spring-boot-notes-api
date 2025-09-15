9d817edad73796eefc8d9543d0dde8366f65da89
Ditract <hgsanez17@gmail.com>
2025-09-14 23:43:53 -0500

    update model validations

--- src/main/java/com/sanez/dto/LoginRequest.java
+++ src/main/java/com/sanez/dto/LoginRequest.java
@@ -1,11 +1,20 @@
 package com.sanez.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequest {
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email debe tener un formato válido")
     private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, max = 64, message = "La contraseña debe tener entre 8 y 64 caracteres")
     private String password;
 }
